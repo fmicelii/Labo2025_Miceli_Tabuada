930942feb2133a3c6bda044ad56aa82c6cbd5aa3
Fran <you@example.com>
2025-04-07 11:32:11 -0300

    ejercicio IntroducirNumerosAmpliado terminado

--- Miceli_Tabuada_java/src/ReemplazoLetra.java
+++ Miceli_Tabuada_java/src/ReemplazoLetra.java
@@ -7,13 +7,13 @@ public class ReemplazoLetra {
         System.out.print("ingrese la letra para reemplazar: ");
         Scanner esc = new Scanner(System.in);
         char letra = esc.next().charAt(0);
-        StringBuilder nuevaFrase = new StringBuilder();
+        String nuevaFrase= "";
         for (int i = 0 ; i < frase.length(); i++){
             char l = frase.charAt(i);
             if ('e' == l){
-                nuevaFrase.append(letra);
+                nuevaFrase += letra;
             }else{
-                nuevaFrase.append(l);
+                nuevaFrase += l;
             }
         }
         System.out.println(nuevaFrase);

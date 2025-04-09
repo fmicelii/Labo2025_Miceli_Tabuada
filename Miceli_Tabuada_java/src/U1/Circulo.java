f230784c447911ffd80234efa7ec86fef70bd41b
Fran <you@example.com>
2025-04-09 09:54:21 -0300

    correcciones en Circulo, comienzo Persona

--- Miceli_Tabuada_java/src/U1/Circulo.java
+++ Miceli_Tabuada_java/src/U1/Circulo.java
@@ -31,19 +31,20 @@ public class Circulo {
 
     //calcular perimetro
     public float calcularPerimetro(float radio){
-        float perimetro = (float) (2 * 3.14F * radio);
+        float perimetro = (2 * 3.14F * radio);
         return perimetro;
     }
 
     //MAIN
-    public static class Main{
-        public static void main(String[] args){
-            Circulo c1 = new Circulo();
+    public static void main(String[] args){
+        Circulo c1 = new Circulo();
+        Circulo c2 = new Circulo(6);
 
-            c1.setRadio(5);
-
-            System.out.println("radio: " + c1.getRadio());
-        }
+        System.out.println("radio c1: " + c1.getRadio());
+        float areac1 = c1.calcularArea(c1.radio);
+        float perimetroc2 = c2.calcularPerimetro(c2.radio);
+        System.out.println("area de c1: " + areac1);
+        System.out.println("perimetro de c2: " + perimetroc2);
     }
 }
 
---
+++ Miceli_Tabuada_java/src/U1/Persona.java
@@ -0,0 +1,14 @@
+package U1;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String direccion;
+
+    public Persona(){
+        this.nombre = "juan";
+        this.edad = 18;
+        this.direccion = "tamborini 3574";
+    }
+
+}

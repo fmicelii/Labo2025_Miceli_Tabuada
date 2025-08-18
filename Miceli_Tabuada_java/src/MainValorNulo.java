4cbedfbcfe5d09b1cd648b21ad970814c4507fc6
Fran <you@example.com>
2025-08-18 12:44:36 -0300

    falta la personalizada

---
+++ Miceli_Tabuada_java/src/MainValorNulo.java
@@ -0,0 +1,64 @@
+public class MainValorNulo {
+    public MainValorNulo() {
+    }
+
+    public void bloqueTryCatch(String nombre){
+        try{
+            //excepcion de una referenciacion a un puntero nulo
+            System.out.println("El largo del nombre es:" + nombre.length());
+        }catch(NullPointerException exc){
+            //capturamos la excepcion
+            System.err.println("se referencia a un puntero nulo, dale un valor man osea bro dale");
+        }
+    }
+
+    public void bloqueThrow(String nombre) throws Exception{
+        try {
+            if (nombre == null){
+                throw new NullPointerException("estas referenciando a una variable NULA tontin");
+            }else {
+                System.out.println("el largo es :" + nombre.length());
+            }
+        }catch (NullPointerException e){
+            System.err.println(e);
+        }
+    }
+
+    public int mostrarLargo(String nombre){
+        return nombre.length();
+    }
+
+    public void bloqueThrows(String nombre)throws Exception{
+        try {
+            System.out.println(mostrarLargo(nombre));
+        }catch (NullPointerException e){
+            System.err.println("el puntero es nulo pa, espabila");
+        }
+    }
+
+    public void bloquePersonalizado(String nombre)throws ValorNuloException{
+        if (nombre == null){
+            throw new ValorNuloException("el valor del nombre es nulo");
+        }else {
+            System.out.println("la cantidad de letras es de:" + nombre.length());
+        }
+
+        try {
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        MainValorNulo vne = new MainValorNulo();
+        String nombre = null;
+
+        vne.bloqueTryCatch(nombre);
+
+        vne.bloqueThrow(nombre);
+
+        vne.bloqueThrows(nombre);
+
+        vne.bloquePersonalizado(nombre);
+    }
+}
+
--- Miceli_Tabuada_java/src/ValorNuloException.java
+++ Miceli_Tabuada_java/src/ValorNuloException.java
@@ -1,23 +1,5 @@
-public class ValorNuloException {
-    public ValorNuloException() {
-    }
-
-    public void bloqueTryCatch(String nombre){
-        try{
-            //excepcion de una referenciacion a un puntero nulo
-            System.out.println("El largo del nombre es:" + nombre.length());
-        }catch(NullPointerException exc){
-            //capturamos la excepcion
-            System.err.println("se referencia a un puntero nulo, dale un valor man osea bro dale");
-        }
-    }
-
-
-    public static void main(String[] args) {
-        ValorNuloException vne = new ValorNuloException();
-        String nombre = null;
-        vne.bloqueTryCatch(nombre);
-
-
+public class ValorNuloException extends Exception {
+    public ValorNuloException(String message) {
+        super(message);
     }
 }
